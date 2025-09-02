51e87ecfafe2f98481f882f449a5ef109ed056ba
Ayush Bhandarkar <163156376+Aayushbhandarkar@users.noreply.github.com>
2025-09-02 08:52:27 +0530

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Nav from '../components/Nav'
 import dp from "../assets/dp.webp"
-import { userDataContext } from '../context/userContext'
+import { userDataContext } from '../context/UserContext'
 import { HiPencil } from "react-icons/hi2"
 import { authDataContext } from '../context/AuthContext'
 import EditProfile from '../components/EditProfile'
