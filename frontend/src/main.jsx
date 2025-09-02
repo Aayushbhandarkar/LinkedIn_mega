2be99af5033840021ffb4c60aa7f539b9f4db977
Ayush Bhandarkar <163156376+Aayushbhandarkar@users.noreply.github.com>
2025-09-02 08:45:36 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AuthContext from "./context/AuthContext.jsx";
-import UserContext from "./context/userContext.jsx";
+import UserContext from "./context/UserContext.jsx";
 import ChatContext from "./context/ChatContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 
